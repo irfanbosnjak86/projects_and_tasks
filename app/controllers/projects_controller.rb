4bea05a5193e57f2871edb5b664fac4637a97dea
mobutuseseku <irfan.bosnjak86@gmail.com>
2017-09-20 17:43:01 +0200

    Adds Create feature for User Projects.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,5 +2,28 @@ class ProjectsController < ApplicationController
   before_action :authenticate_user!
   
   def index
+    @projects = Project.all
+  end
+
+  def new
+    @project = current_user.projects.new 
+  end
+
+  def create
+    @project = current_user.projects.new(project_params)
+    
+    if @project.save 
+      flash[:notice] = "Project has been created"
+      redirect_to root_path
+    else 
+      flash[:error] = "Project has not been created"
+      render :new
+    end 
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:title, :description, :user_id)
   end
 end
